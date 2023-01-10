636c1de8fecd1ec0f10f364ec7769bcf41d0a93f
許呈善 Albert Hsu <pt019@cathaysite.com.tw>
2023-01-10 09:51:33 +0800

    feat: 透過Visual Studio Code快速產生POCOs
    因應新需求做調整：
     將原有在LINQPad上產生POCOs的程式, 改成可以直接使用VSCode完成
    
    調整項目：
    1. 新增Console App - SQL POCOs Builder
    2. 於Program.cs呼叫的先前的generate-c-pocos-from-sql-statement-in-linqpad的程式, 並顯示至Console
    
    參考
    https://dotblogs.com.tw/kkman021/2015/12/13/004404
    http://www.necronet.org/archive/2012/10/09/generate-c-pocos-from-sql-statement-in-linqpad.aspx

---
+++ GenerateCSharpPOCOsFromSQLStatement/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenerateCSharpPOCOsFromSQLStatement;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+// 連線字串
+DbConnection _dbConnection = new SqlConnection(@"[ConnectionString]");
+
+// 資料表名稱
+var nameOfTableAndClass = "[ClassName]";
+
+// 這邊修改為您要執行的 SQL Command
+var sqlCommand = $@"SELECT [Columns] FROM [Table]";
+
+// 在 DumpClass 方法裡放 SQL Command 和 Class 名稱
+var value = _dbConnection.DumpClass(sqlCommand.ToString(), nameOfTableAndClass);
+
+Console.WriteLine(value);
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
